c1181369d0f4f5afd3d75c12f55868b936321229
HamdiEzzahra <hidantoure@gmail.com>
2022-08-04 17:30:39 +0100

    added MovieRemoteDataSource class to dataLayer

---
+++ lib/movies_module/data/datasource/movie_remote_data_source.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:entertainment_app/movies_module/data/models/movie_model.dart';
+
+class MovieRemoteDataSource {
+  Future<List<MovieModel>> getNowPlayingMovies() async {
+    final response = await Dio().get(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=72b66dbbb9816e3f20405337150e741a");
+
+    if (response.statusCode == 200) {
+      return List<MovieModel>.from((response.data["results"] as List)
+          .map((e) => MovieModel.fromJson(e)));
+    } else {
+      return [];
+    }
+  }
+}
