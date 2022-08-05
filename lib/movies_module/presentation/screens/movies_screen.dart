a33c95443877f342598940e86d952e6760763412
HamdiEzzahra <hidantoure@gmail.com>
2022-08-05 14:44:58 +0100

    created movie screen

---
+++ lib/movies_module/presentation/screens/movies_screen.dart
@@ -0,0 +1,14 @@
+import 'package:entertainment_app/movies_module/data/datasource/movie_remote_data_source.dart';
+import 'package:entertainment_app/movies_module/data/repository/movies_repository.dart';
+import 'package:entertainment_app/movies_module/domain/repository/base_movies_repository.dart';
+import 'package:entertainment_app/movies_module/domain/usecase/get_now_playing_movies_usecase.dart';
+import 'package:flutter/material.dart';
+
+class MoviesScreen extends StatelessWidget {
+  const MoviesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
