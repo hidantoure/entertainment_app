9ca91783bcd7485bbbd795f3817feb8aac65c96b
HamdiEzzahra <hidantoure@gmail.com>
2022-08-08 13:31:49 +0100

    + blocbuilder to popular  component

--- lib/movies_module/presentation/components/popular_component.dart
+++ lib/movies_module/presentation/components/popular_component.dart
@@ -1,6 +1,10 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:entertainment_app/core/utils/enums.dart';
+import 'package:entertainment_app/movies_module/presentation/controller/movies_bloc.dart';
+import 'package:entertainment_app/movies_module/presentation/controller/movies_states.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shimmer/shimmer.dart';
 
 import '../../../core/utils/app_constants.dart';
@@ -11,50 +15,74 @@ class PopularMovieComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FadeIn(
-      duration: const Duration(milliseconds: 500),
-      child: SizedBox(
-        height: 170.0,
-        child: ListView.builder(
-          shrinkWrap: true,
-          scrollDirection: Axis.horizontal,
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          itemCount: moviesList.length,
-          itemBuilder: (context, index) {
-            final movie = moviesList[index];
-            return Container(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: InkWell(
-                onTap: () {
-                  /// TODO : NAVIGATE TO  MOVIE DETAILS
-                },
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-                  child: CachedNetworkImage(
-                    width: 120.0,
-                    fit: BoxFit.cover,
-                    imageUrl: AppContants.imageUrl(movie.backdropPath),
-                    placeholder: (context, url) => Shimmer.fromColors(
-                      baseColor: Colors.grey[850]!,
-                      highlightColor: Colors.grey[800]!,
-                      child: Container(
-                        height: 170.0,
-                        width: 120.0,
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(8.0),
+    return BlocBuilder<MoviesBloc, MoviesState>(
+        buildWhen: (previous, current) =>
+            previous.popularMoviesState != current.popularMoviesState,
+        builder: (context, state) {
+          switch (state.popularMoviesState) {
+            case RequestState.loading:
+              return const SizedBox(
+                height: 170,
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            case RequestState.loaded:
+              return FadeIn(
+                duration: const Duration(milliseconds: 500),
+                child: SizedBox(
+                  height: 170.0,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    itemCount: state.popularMovies.length,
+                    itemBuilder: (context, index) {
+                      final movie = state.popularMovies[index];
+                      return Container(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: InkWell(
+                          onTap: () {
+                            /// TODO : NAVIGATE TO  MOVIE DETAILS
+                          },
+                          child: ClipRRect(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(8.0)),
+                            child: CachedNetworkImage(
+                              width: 120.0,
+                              fit: BoxFit.cover,
+                              imageUrl:
+                                  AppContants.imageUrl(movie.backdropPath),
+                              placeholder: (context, url) => Shimmer.fromColors(
+                                baseColor: Colors.grey[850]!,
+                                highlightColor: Colors.grey[800]!,
+                                child: Container(
+                                  height: 170.0,
+                                  width: 120.0,
+                                  decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.circular(8.0),
+                                  ),
+                                ),
+                              ),
+                              errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
+                      );
+                    },
                   ),
                 ),
-              ),
-            );
-          },
-        ),
-      ),
-    );
+              );
+            case RequestState.error:
+              return SizedBox(
+                height: 170,
+                child: Center(
+                  child: Text(state.nowPlayingMessage),
+                ),
+              );
+          }
+        });
   }
 }
