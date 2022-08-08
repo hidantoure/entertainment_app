9708f62f786081d7f30d9c68c4a485c35ca9e54c
HamdiEzzahra <hidantoure@gmail.com>
2022-08-08 17:56:38 +0100

    movie detail model

---
+++ lib/movies_module/data/models/movie_details_model.dart
@@ -0,0 +1,26 @@
+import 'package:entertainment_app/movies_module/data/models/genres_model.dart';
+import 'package:entertainment_app/movies_module/domain/entities/movie_detail.dart';
+
+class MovieDetailsModel extends MovieDetail {
+  const MovieDetailsModel({
+    required super.backdropPath,
+    required super.genres,
+    required super.id,
+    required super.overview,
+    required super.releaseDate,
+    required super.runtime,
+    required super.title,
+    required super.voteAverage,
+  });
+  factory MovieDetailsModel.fromJson(Map<String, dynamic> json) =>
+      MovieDetailsModel(
+        backdropPath: json["backdrop_path"],
+        genres: List.from(json['gernes'].map((x) => GenresModel.fromJson(x))),
+        id: json['id'],
+        overview: json['overview'],
+        releaseDate: json['release_date'],
+        runtime: json['runtime'],
+        title: json['title'],
+        voteAverage: json['vote_average'],
+      );
+}
